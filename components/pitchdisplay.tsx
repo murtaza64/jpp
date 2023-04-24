a8a14d64420584111b5c960db071424eeb2b0c6a
Murtaza Javaid <murtaza.javaid@gmail.com>
2023-04-24 17:27:19 -0400

    Basic mic widget and integration with koutei API

--- components/pitchdisplay.tsx
+++ components/pitchdisplay.tsx
@@ -2,6 +2,8 @@ import React, { ReactNode, useEffect, useRef, useState } from "react"
 import styles from "../styles/PitchDisplay.module.css"
 import wordjson from "../public/words.json"
 import { WordMap, WordMapEntry } from "../wordMap";
+import { json } from "stream/consumers";
+import { JsxElement, JsxText } from "typescript";
 
 function getCssProperty(property: string) {
   return getComputedStyle(document.documentElement)
@@ -11,6 +13,143 @@ function getCssProperty(property: string) {
 type CurrentWordProps = {
   word: string
   wordmapsubset: WordMap
+}
+
+type RecorderProps = {
+  word: string
+}
+
+function StudentAttempt(
+  { 
+    word
+  }: RecorderProps
+) {
+  const mimeType = "audio/webm";
+  const [permission, setPermission] = useState(false);
+  const [stream, setStream] = useState<MediaStream | undefined>(undefined);
+  const mediaRecorder = useRef<MediaRecorder | null>(null);
+  const [recordingStatus, setRecordingStatus] = useState("inactive");
+  const [audioChunks, setAudioChunks] = useState<Blob[]>([]);
+  const [audio, setAudio] = useState("");
+  const [permissionRejected, setPermissionRejected] = useState(false);
+  const feedbackBox = useRef<HTMLDivElement>(null);
+  const [feedbackBoxContent, setFeedbackBoxContent] = useState<string>("Try it yourself!");
+
+  const getMediaPermission = async () => {
+    if ("MediaRecorder" in window) {
+      try {
+        const streamData = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: false,
+        });
+        setPermission(true);
+        setPermissionRejected(false);
+        setStream(streamData);
+
+      }
+      catch (e) {
+        console.log("permission rejected")
+        setPermissionRejected(true);
+      }
+    } else {
+        alert("The MediaRecorder API is not supported in your browser.");
+    }
+  }
+
+  const micButtonClick = async () => {
+    if (!permission) {
+      getMediaPermission();
+    }
+    else if (recordingStatus === "inactive") {
+      startRecording();
+    }
+    else {
+      stopRecording();
+    }
+  };
+
+  const startRecording = async () => {
+    //create new Media recorder instance using the stream
+    if (stream === undefined) {
+      return
+    }
+    setRecordingStatus("recording");
+    console.log("recording")
+    const media = new MediaRecorder(stream, { mimeType: mimeType });
+    //set the MediaRecorder instance to the mediaRecorder ref
+    mediaRecorder.current = media;
+    //invokes the start method to start the recording process
+    mediaRecorder.current.start();
+    let localAudioChunks: Blob[] = [];
+    mediaRecorder.current.ondataavailable = (event) => {
+      if (typeof event.data === "undefined") return;
+      if (event.data.size === 0) return;
+      localAudioChunks.push(event.data);
+    };
+    setAudioChunks(localAudioChunks);
+  };
+
+  const stopRecording = () => {
+    //stops the recording instance
+    if (mediaRecorder.current === null) {
+      return
+    }
+    setRecordingStatus("inactive");
+    console.log("stop recording")
+    mediaRecorder.current.stop();
+    mediaRecorder.current.onstop = () => {
+      //creates a blob file from the audiochunks data
+      const audioBlob = new Blob(audioChunks, { type: mimeType });
+      //creates a playable URL from the blob file.
+      const reader = new FileReader();
+      reader.addEventListener("loadend", async () => {
+        // reader.result contains the contents of blob as a typed array
+        console.log(reader.result);
+        const response = await fetch("http://localhost:5000/check", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json" 
+          },
+          body: JSON.stringify({
+            "word": word,
+            "audio": reader.result
+          })
+        });
+        const result = await response.json();
+        console.log(result);
+        const roundedScore = Math.round(result["score"]);
+        setFeedbackBoxContent("Score: " + roundedScore.toString());
+      });
+      reader.readAsDataURL(audioBlob);
+
+      const audioUrl = URL.createObjectURL(audioBlob);
+      setAudio(audioUrl);
+      setAudioChunks([]);
+      console.log(audioUrl);
+    };
+  };
+
+  let buttonClassName = styles.recordButton;
+  if (recordingStatus === "recording") {
+    buttonClassName += " " + styles.recording
+  }
+
+  // getMediaPermission();
+
+  return (
+    <div className={styles.studentAttempt}>
+      <button onClick={micButtonClick} className={buttonClassName}> <img src="/mic.svg" width="20px" height="20px"/> </button>
+      { permissionRejected? (
+        <div className={styles.feedbackBox}> To try it yourself, you need to provide mic access </div>
+      ) : (
+        <div className={styles.feedbackBox} ref={feedbackBox}> {feedbackBoxContent} </div>)
+      }
+
+      {/* {audio ? (
+        <audio src={audio} controls></audio>
+      ) : null} */}
+    </div>
+  )
 }
 
 function CurrentWord(
@@ -105,6 +244,9 @@ function CurrentWord(
       {/* <div className={styles.japanese}>{word}</div> */}
       <canvas className={styles.maruPlot} ref={canvasRef}/>
       <div className={styles.english}>{wordmapsubset.get(word)?.english}</div>
+      <StudentAttempt
+        word={word}
+      />
     </div>
   )
 }
